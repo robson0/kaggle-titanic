6917aa8aaf9791bfdf86ae9ce63d7a20c1ba4eff
Robson Edwards <robson.edwards@gmail.com>
2019-01-10 14:59:05 -0800

    Comment out stuff I don't like

--- Scripts/Titanic.R
+++ Scripts/Titanic.R
@@ -3,8 +3,8 @@
 ##################################
 
 # ensure all packages are installed
-if (!require("pacman")) install.packages("pacman")
-pacman::p_load(readr)
+#if (!require("pacman")) install.packages("pacman")
+#pacman::p_load(readr)
 
 library('readr')
 
@@ -12,17 +12,17 @@ library('readr')
 train <- read_csv('Data/train.csv')
 test  <- read_csv('Data/test.csv')
 
-# join datasets
-train$set <- "train"
-test$set  <- "test"
-test$Survived <- NA
-full <- rbind(train, test)
+# join datasets # I don't think you're supposed to do this
+#train$set <- "train"
+#test$set  <- "test"
+#test$Survived <- NA
+#full <- rbind(train, test)
 
-summary(full)
-missing_per_column <- colSums(sapply(full, is.na))
-missing_per_column <- as.data.frame(missing_per_column)
+#summary(full)
+#missing_per_column <- colSums(sapply(full, is.na))
+#missing_per_column <- as.data.frame(missing_per_column)
 
-comp.train <- train[complete.cases(train),]
+#comp.train <- train[complete.cases(train),]
 
-model1 <- glm(Survived ~ Pclass + Sex + Age, family = "binomial", data = comp.train)
-summary(model1)
+#model1 <- glm(Survived ~ Pclass + Sex + Age, family = "binomial", data = comp.train)
+#summary(model1)
