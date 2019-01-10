106577d1080a9da88a4dd6a6c1a6177d26913de1
Robson Edwards <robson.edwards@gmail.com>
2019-01-10 14:59:27 -0800

    clean up

--- Scripts/Titanic.R
+++ Scripts/Titanic.R
@@ -1,28 +1,5 @@
-##################################
-# LOGISTIC REGRESSION
-##################################
-
-# ensure all packages are installed
-#if (!require("pacman")) install.packages("pacman")
-#pacman::p_load(readr)
-
 library('readr')
 
 # import data
 train <- read_csv('Data/train.csv')
 test  <- read_csv('Data/test.csv')
-
-# join datasets # I don't think you're supposed to do this
-#train$set <- "train"
-#test$set  <- "test"
-#test$Survived <- NA
-#full <- rbind(train, test)
-
-#summary(full)
-#missing_per_column <- colSums(sapply(full, is.na))
-#missing_per_column <- as.data.frame(missing_per_column)
-
-#comp.train <- train[complete.cases(train),]
-
-#model1 <- glm(Survived ~ Pclass + Sex + Age, family = "binomial", data = comp.train)
-#summary(model1)
