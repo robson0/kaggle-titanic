16683ac2b81703b15d184d390ca1415ad585f802
Robson Edwards <robson.edwards@gmail.com>
2019-01-10 17:02:40 -0800

    Setup

--- Scripts/Titanic.R
+++ Scripts/Titanic.R
@@ -1,5 +1,21 @@
-library('readr')
+require('readr') 
+require('dplyr')
 
 # import data
-train <- read_csv('Data/train.csv')
-test  <- read_csv('Data/test.csv')
+data <- read_csv('Data/train.csv')
+# Randomly split into train and test. We can't use the given test data bc it 
+# doesn't come with labels. 
+test.indices <- sample(nrows(data), 200)
+# TODO: finish this 
+
+# Fit a model with all relevant variables/columns...
+# Omitting Name, Ticket number, and Cabin for now as they are somewhat arbitrary
+full.model <- glm(Survived ~ . , family = binomial, 
+                  data = select(train, -Name, -Ticket, -Cabin))
+predictions <- predict(full.model, select(test, -Name, -Ticket, -Cabin))
+
+# TODO: Assess model accuracy
+
+# TODO: other diagnostics
+
+# TODO: The same for more models 
